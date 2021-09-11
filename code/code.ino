eea83b590b7a1919d58952044a8a603cdaece1f0
MrSuradechTH <53972303+MrSuradechTH@users.noreply.github.com>
2021-09-11 10:38:12 +0700

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,44 +1,54 @@
 //motor
-const uint8_t motor_row = 6,motor_col = 3;
-uint8_t motor[motor_row][motor_col] = {{32, 33, 4},{34, 35, 6},{36, 37, 8},{48, 49, 10},{50, 51, 12},{52, 53}}; //ล้อซ้าย,ล้อขวา,แขนซ้าย,แขนขวา,?,? (มองจากด้านหลัง)
-uint8_t motor_speed[] = {255,255,255,255,255,255};
+const uint8_t motor_row = 4,motor_col = 3;
+uint8_t motor[motor_row][motor_col] = {{32, 33, 4},{34, 35, 6},{36, 37, 8},{48, 49, 10}}; //ล้อซ้าย,ล้อขวา,แขนซ้าย,แขนขวา (มองจากด้านหลัง)
+const uint8_t motor_speed_row = 5,motor_speed_col = 2;
+uint8_t motor_speed[motor_speed_row][motor_speed_col] = {{255,255},{255,255},{0,255},{255,0},{0,0},}; //เดินหน้า,ถอยหลัง,เลี้ยวซ้าย,เลี้ยวขวา,หยุด (มอเตอร์ซ้าย,มอเตอร์ขวา)
+uint8_t motor_delay[] = {0,0,1000,1000,0}; //เดินหน้า,ถอยหลัง,เลี้ยวซ้าย,เลี้ยวขวา,หยุด
 
 //line_sensor
 uint8_t line_sensor[] = {14,15,16,17,18,19,22};
-String line_status,line_status_old;
+String line_status; //,line_status_old
 
 //other
 uint8_t stack;
 //String direction_controling = ""; //forward,backward,right,left,opposite (not yet used)
 
 //robot
-void robot_motor(uint8_t digital_a,uint8_t digital_b,uint8_t digital_c,uint8_t digital_d,uint8_t analog_a,uint8_t analog_b) {
+void robot_motor(uint8_t digital_a,uint8_t digital_b,uint8_t digital_c,uint8_t digital_d,uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
   digitalWrite(motor[0][0], digital_a);
   digitalWrite(motor[0][1], digital_b);
   digitalWrite(motor[1][0], digital_c);
   digitalWrite(motor[1][1], digital_d);
   analogWrite(motor[0][2], analog_a);
   analogWrite(motor[1][2], analog_b);
+  if (int_a != 0) {
+    delay(int_a);
+    robot_stop(motor_speed[4][0],motor_speed[4][1],motor_delay[4]);
+  }
 }
 
-void robot_forward(uint8_t analog_a,uint8_t analog_b) {
-  robot_motor(1,0,1,0,analog_a,analog_b);
+void robot_forward(uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
+  robot_motor(1,0,1,0,analog_a,analog_b,int_a);
 }
 
-void robot_backward(uint8_t analog_a,uint8_t analog_b) {
-  robot_motor(0,1,0,1,analog_a,analog_b);
+void robot_backward(uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
+  robot_motor(0,1,0,1,analog_a,analog_b,int_a);
 }
 
-void robot_right(uint8_t analog_a,uint8_t analog_b) {
-  robot_motor(1,0,0,1,analog_a,analog_b);
+void robot_right(uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
+  robot_motor(1,0,0,1,analog_a,analog_b,int_a);
 }
 
-void robot_left(uint8_t analog_a,uint8_t analog_b) {
-  robot_motor(0,1,1,0,analog_a,analog_b);
+void robot_left(uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
+  robot_motor(0,1,1,0,analog_a,analog_b,int_a);
 }
 
-void robot_opposite(uint8_t analog_a,uint8_t analog_b) {
-  robot_motor(1,0,0,1,analog_a,analog_b);
+void robot_opposite(uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
+  robot_motor(1,0,0,1,analog_a,analog_b,int_a);
+}
+
+void robot_stop(uint8_t analog_a,uint8_t analog_b,uint16_t int_a) {
+  robot_motor(0,0,0,0,analog_a,analog_b,int_a);
 }
 
 //arm
@@ -60,7 +70,7 @@ void arm_down(uint8_t analog_a,uint8_t analog_b) {
 }
 
 void line_check() {
-  line_status_old = line_status;
+  //line_status_old = line_status;
   line_status = "";
   for(uint8_t c = 0;c < 7;c++) {
     line_status += String(digitalRead(line_sensor[c]));
