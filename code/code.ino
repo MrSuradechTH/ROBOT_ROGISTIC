c14162e6aed843c64a0c04cd1d7010f7354e9c15
MrSuradechTH <53972303+MrSuradechTH@users.noreply.github.com>
2021-09-12 17:21:56 +0700

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -230,13 +230,12 @@ String get_color() {
     }else {
         color_stack_end[a] = "unknow";
     }
-    
   }
   for (uint8_t a = 0;a < color_all;a++) {
     if (color_stack_end[a] != "unknow") {
       color = color_stack_end[a];
       break;
-    }else if (a == color_all && color_stack_end[a] == "unknow") {
+    }else if (a == color_all-1 && color_stack_end[a] == "unknow") {
       color = "unknow";
     }
   }
